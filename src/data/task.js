7d6b8c1a1ced3791f0242bd696c2483fc1f8cc6f
aley jose <aleyjose755@gmail.com>
2025-08-29 12:07:32 -0300

    error title

--- src/data/task.js
+++ src/data/task.js
@@ -1,19 +1,19 @@
 export const tasks = [
   {
     id: 0,
-    titel: "mi primera tarea",
+    title: "mi primera tarea",
     description: "realizar la tarea 1",
   },
 
   {
     id: 1,
-    titel: "mi segunda tarea",
+    title: "mi segunda tarea",
     description: "realizar la tarea 2 ",
   },
 
   {
     id: 2,
-    titel: "mi tercera tarea",
+    title: "mi tercera tarea",
     description: "realizar la tarea 3 para finalizar el curso",
   },
 ];
